d9021e1f21a118bfcdd722df6a3de5764b195cc1
dnemens <35116826+dnemens@users.noreply.github.com>
2018-09-11 17:16:30 -0700

    yep

--- scripts/plotting.R
+++ scripts/plotting.R
@@ -1,6 +1,7 @@
 #plotz!###
 
 oaks <- read.csv("C:/Users/dnemens/Dropbox/OWO/white-oak/data sheets/post_merge_forR.csv")
+library(tidyverse)
 #run pca script first!!  
 oaks.scores <- data.frame(oaks, scores)
 ####################################
@@ -32,18 +33,26 @@ sizet <- try$scores
 htt <- try$height.m
 dbht <- try$DBH.cm
 
+#lm for effect of ht on dieback
+q <- lm(diebackt~htt)
+
 ggplot(try, aes(x=htt, y=diebackt))+
   geom_point (aes(), position="jitter", cex=(sqrt(dbht)*3), pch=21, fill="darkorange", alpha=.6)+
   theme_bw()+
-  #scale_fill_gradient(low="yellow", high = "darkorange", limits = c(2,18), breaks = c(2, 10, 18), labels= c("0",10,"18")) + 
-  labs(x = "Height (m)", y="Crown dieback (%)")+
-  theme(axis.title = element_text(size=16), panel.grid = element_blank(), axis.text = element_text(color = "black", size=10))+
-  #labs(fill = "Height (m)")+
-  theme(legend.position = c(1,.67), legend.justification = c(1,0))+
-  theme(legend.key = element_rect(colour = 'black', linetype = "solid"))+
-  ylim(-6, 105)+
-  xlim(0,20)
-  #annotate ("segment", x=-3, xend = 3, y=-5, yend=-5, colour = "grey", alpha = .8, size=6)
-
-  #aes(fill="vector")
+   labs(x = "Height (m)", y="Crown dieback (%)")+
+  theme(axis.title = element_text(size=16), panel.grid = element_blank(), 
+        axis.text = element_text(color = "black", size=10))+
+  geom_smooth(method = lm,  formula = y ~x, fullrange = T, lwd=1.3)+
+  xlim(0,18)+
+  annotate("text", x = 15, y = 100, label = "italic(R) ^ 2 == 0.24", parse = TRUE, size=4)
+ 
 ggsave("C:/Users/dnemens/Dropbox/OWO/plots/scorched_size.tiff", device = "tiff", width = 6, height = 4.5, units = "in", dpi = 800)
+
+
+#labs(fill = "Height (m)")+
+#scale_fill_gradient(low="yellow", high = "darkorange", limits = c(2,18), breaks = c(2, 10, 18), labels= c("0",10,"18")) + 
+#theme(legend.position = c(1,.67), legend.justification = c(1,0))+
+#  theme(legend.key = element_rect(colour = 'black', linetype = "solid"))+
+  #aes(fill="vector")
+#annotate ("segment", x=-3, xend = 3, y=-5, yend=-5, colour = "grey", alpha = .8, size=6)
+
